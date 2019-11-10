271bfdc96a306af86f5bf080abcb0c3a76522c57
RigoLigoRLC <rigoligo03@gmail.com>
2019-11-10 23:57:11 +0800

    added pcb pad header basics

--- inc/elements.hpp
+++ inc/elements.hpp
@@ -4,13 +4,46 @@
   #include <consts.hpp>
   namespace lc2kicad
   {
+    struct coordinates
+    {
+      float X;
+      float Y;
+    };
+    typedef coordinates sizeXY;
+
     class PCBElements
     {
       private:
-        int LCEDA_Layer;
-
+        bool isVisible;
       public:
-        void setLCLayer(int LCLayer) {LCEDA_Layer = LCLayer;};
+        bool getVisibility() { return isVisible; };
+        bool setVisibility(bool visibility) { isVisible = visibility; };
+        int  outputToKiCadFile(fstream &kicadFile);
+    };
+
+    class PCB_Pad : public PCBElements
+    {
+      private:
+        int  padShape, holeShape, padType, orientation;
+        coordinates padCoordinates;
+        sizeXY padSize, holeSize;
+      public:
+        vector<coordinates> shapePolygonPoints;
+        void setPadCoordinate(float X, float Y) { padCoordinates.X = X; padCoordinates.Y = Y; };
+        void setPadSize(float X, float Y) { padSize.X = X; padSize.Y = Y; };
+        void setHoleSize(float X, float Y) { holeSize.X = X; holeSize.Y = Y; };
+        void setPadRotation(float rotation) { orientation = rotation; };
+        void setPadShape(int shape) { padShape = shape; };
+        void setPadType(int type) { padType = type; };
+        void setHoleShape(int shape) { holeShape = shape; };
+
+        const coordinates &getPadCoordinate() { return padCoordinates; };
+        const sizeXY &setPadSize() { return padSize; };
+        const sizeXY &setHoleSize() { return holeSize; };
+        float getPadRotation() { return orientation; };
+        int getPadShape() { return padShape; };
+        int getPadType() { return padType; };
+        int getHoleShape() { return holeShape; };
     };
   }
 #endif
